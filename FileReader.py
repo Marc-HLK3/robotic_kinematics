bac07f3e1bcc8f8fb58be5b739ef33b17c2cb45a
hml7rng <marc.halschka@gmail.com>
2023-08-13 13:47:58 +0200

    new file: FileReader, module to read datasets as pdf

---
+++ FileReader.py
@@ -0,0 +1,12 @@
+# import pandas as pd
+class FileReader():
+    def __init__(self, file_path):
+        self.file_path = file_path
+    
+    def pdf(self):
+        try:
+            pdf = pd.read_csv(self.file_path)
+            return pdf
+        except Exception as e:
+            print(f"Error reading the file: {e}")
+            return None
