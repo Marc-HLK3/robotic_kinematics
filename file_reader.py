6e3030dd2bfd4e2dd1a55700927049f524f08b70
hml7rng <marc.halschka@gmail.com>
2023-08-13 14:18:22 +0200

    added .env file, for environment variables

--- file_reader.py
+++ file_reader.py
@@ -1,8 +1,17 @@
+"""
+This module provides a FileReader class to read data from various file formats.
+"""
+
+# pylint: disable=import-error
 import pandas as pd
-class FileReader():
+
+
+class FileReader:
+    """This module loads files as pdf, np array, etc."""
+
     def __init__(self, file_path):
         self.file_path = file_path
-    
+
     def pdf(self):
         try:
             pdf = pd.read_csv(self.file_path)
@@ -10,3 +19,9 @@ class FileReader():
         except Exception as e:
             print(f"Error reading the file: {e}")
             return None
+
+
+if __name__ == "__main__":
+
+    reader = FileReader(file_path="test.csv")
+    data = reader.read_csv()
