2a4b861d8d1f94a851c0670d061d8102e0f88299
hml7rng <marc.halschka@gmail.com>
2023-08-13 14:30:27 +0200

    removed debbuging statement

--- file_reader.py
+++ file_reader.py
@@ -3,25 +3,25 @@ This module provides a FileReader class to read data from various file formats.
 """
 
 # pylint: disable=import-error
+import os
 import pandas as pd
+from dotenv import load_dotenv
 
 
 class FileReader:
     """This module loads files as pdf, np array, etc."""
 
-    def __init__(self, file_path):
+    def __init__(self, file_path=None):
         self.file_path = file_path
+        if self.file_path is None:
+            load_dotenv()
+            self.file_path = os.getenv("dataset_path")
 
-    def pdf(self):
+    def read_csv_pdf(self):
+        """Read csv file as Pandas dataframe."""
         try:
             pdf = pd.read_csv(self.file_path)
             return pdf
-        except Exception as e:
-            print(f"Error reading the file: {e}")
+        except Exception as exeption:
+            print(f"Error reading the file: {exeption}")
             return None
-
-
-if __name__ == "__main__":
-
-    reader = FileReader(file_path="test.csv")
-    data = reader.read_csv()
